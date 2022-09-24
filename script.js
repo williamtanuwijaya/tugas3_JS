b7855e0f32c9d3edaef6afb1ae2eb221ab0ae4f8
William  Tanuwijaya <will.tw0807@gmail.com>
2022-09-24 23:10:17 +0700

    revisi file JS dan HTML

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const bgColorSelector = document.getElementById('bg-color')
 const textColorSelector = document.getElementById('teks-color')
 const alignSelector = document.getElementById("textarea")
 
-bgColorSelector.addEventListener('input',changeBackground,false)
-textColorSelector.addEventListener('input',changeTextColor,false)
+bgColorSelector.addEventListener('input', changeBackground, false)
+textColorSelector.addEventListener('input', changeTextColor, false)
 // alignSelector.addEventListener('onChange',change,fal)
 
 
@@ -12,13 +12,13 @@ function changeBackground() {
    document.body.style.backgroundColor = colorBg;
 }
 
-function changeTextColor(){
-let textColor = document.getElementById('teks-color').value
-document.getElementById('sampleText').style.color = textColor
+function changeTextColor() {
+   let textColor = document.getElementById('teks-color').value
+   document.getElementById('sampleText').style.color = textColor
 }
 
 document.getElementById("random-number").innerHTML =
-Math.floor(Math.random() * 11);
+   Math.floor(Math.random() * 11);
 
 // function myFunction() {
 //    document.getElementById("kiri").style.textAlign = "lef";
@@ -35,4 +35,4 @@ Math.floor(Math.random() * 11);
 function change(item) {
    var name = item.options[item.selectedIndex].text
    document.getElementById("textarea").style.textAlign = name;
- }
+}
