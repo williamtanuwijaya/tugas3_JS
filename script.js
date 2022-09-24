a42c7e093b2211eecdf215860d66052eac906e73
William  Tanuwijaya <will.tw0807@gmail.com>
2022-09-24 11:10:38 +0700

    revisi di file html dan js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const bgColorSelector = document.getElementById('bg-color')
 const textColorSelector = document.getElementById('teks-color')
-
+const alignSelector = document.getElementById("textarea")
 
 bgColorSelector.addEventListener('input',changeBackground,false)
 textColorSelector.addEventListener('input',changeTextColor,false)
+// alignSelector.addEventListener('onChange',change,fal)
+
 
 function changeBackground() {
    let colorBg = document.getElementById('bg-color').value;
@@ -14,3 +16,23 @@ function changeTextColor(){
 let textColor = document.getElementById('teks-color').value
 document.getElementById('sampleText').style.color = textColor
 }
+
+document.getElementById("random-number").innerHTML =
+Math.floor(Math.random() * 11);
+
+// function myFunction() {
+//    document.getElementById("kiri").style.textAlign = "lef";
+//  }
+
+//  function rightFunction() {
+//    document.getElementById("kanan").style.textAlign = "right";
+//  }
+
+//  function centerFunction() {
+//    document.getElementById("tengah").style.textAlign = "center";
+//  }
+
+function change(item) {
+   var name = item.options[item.selectedIndex].text
+   document.getElementById("textarea").style.textAlign = name;
+ }
